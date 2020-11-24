3bc6c7d45f9704fd2e01404f34e3b7383066ec77
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2020-11-24 20:05:12 -0300

    File and functions organization

--- app.py
+++ app.py
@@ -3,11 +3,20 @@ import logging
 import time
 from datetime import timedelta
 from threading import ThreadError, Thread
+from string import punctuation
 
 # Added modules
 from pytictoc import TicToc
+import enchant
+import nltk
+from nltk.corpus import stopwords
+import enchant
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 # Project modules
+from replacers import RegexpReplacer
+
 
 # Print in file
 # logging.basicConfig(filename='logs.log',
@@ -22,6 +31,24 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%d/%b/%Y - %H:%M:%S')
 
 logger = logging.getLogger(__name__)
+
+
+class LanguageProcessor(object):
+    """
+    Natural language processor package initializer
+    """
+
+    def __init__(self):
+        """
+        Download or/and update the language-neutral sentence segmentation tool
+        """
+        nltk.download('punkt')
+        nltk.download('stopwords')
+
+        # Init dicts and english stopwords
+        self.replacer = RegexpReplacer()
+        self.word_dict = enchant.Dict("en_US")
+        self.stops = set(stopwords.words('english'))
 
 
 class ElapsedTime(object):
@@ -71,3 +98,47 @@ def run(interval):
 def application():
     """" All application has its initialization from here """
     logger.info('Main application is running!')
+
+    # NLTK initializer
+    nlp = LanguageProcessor()
+
+
+def nltk_ignition():
+    global replacer
+    global word_dict
+    global stops
+
+    replacer = RegexpReplacer()
+    word_dict = enchant.Dict("en_US")
+    stops = set(stopwords.words('english'))
+    return None
+
+
+def dealing_with_stopwords(string):
+    string_without_stopwords = ''
+    string_without_punctuation = ''
+    words = word_tokenize(string)
+
+    for i in words:
+        if i not in punctuation:
+            if word_dict.check(i):
+                string_without_punctuation = string_without_punctuation + i + ' '
+                if i not in stops:
+                    string_without_stopwords = string_without_stopwords + i + ' '
+
+    if len(string_without_stopwords) is 0:
+        return string_without_punctuation[0:(len(string_without_punctuation) - 1)]
+    else:
+        return string_without_stopwords[0:(len(string_without_stopwords) - 1)]
+
+
+def remove_punctuation(content_with_punctuation):
+    w = word_tokenize(content_with_punctuation)
+    concat = ''
+    for j in w:
+        if j not in punctuation:
+            concat = concat + j + ' '
+
+    content_without_punctuation = concat[0:(len(concat) - 1)]
+    return content_without_punctuation
+
