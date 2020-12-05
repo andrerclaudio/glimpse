f9bd202ee5542c2ac45596fa74ac8444ea6452ac
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2020-12-04 22:06:46 -0300

    Correlation

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from threading import ThreadError, Thread
 from string import punctuation
 
 # Added modules
+import pandas as pd
+from scipy.spatial import distance_matrix
+from scipy.spatial.distance import pdist, squareform
+import matplotlib.pyplot as plt
+import seaborn as sns
 from pytictoc import TicToc
 import enchant
 import nltk
@@ -16,7 +21,6 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 
 # Project modules
 from replacers import RegexpReplacer
-
 
 # Print in file
 # logging.basicConfig(filename='logs.log',
@@ -55,6 +59,7 @@ class ElapsedTime(object):
     """
     Measure the elapsed time between Tic and Toc
     """
+
     def __init__(self):
         self.t = TicToc()
         self.t.tic()
@@ -99,8 +104,61 @@ def application():
     """" All application has its initialization from here """
     logger.info('Main application is running!')
 
-    # NLTK initializer
-    nlp = LanguageProcessor()
+    devices = {
+        'a': [1, 1, 1, 1],
+        'b': [2, 2, 2, 2],
+        'c': [3, 3, 2, 3],
+        'd': [4, 4, 4, 4],
+        'e': [5, 5, 5, 5]
+    }
+
+    # Create DataFrame
+    devices_dataframe = pd.DataFrame.from_dict(devices)
+    # Rows names
+    devices_dataframe.index = ["Sensibilidade", "Acionamento", "Tipo", "Indicação"]
+
+    df_euclid = pd.DataFrame(
+        1 / (1 + distance_matrix(devices_dataframe.T, devices_dataframe.T)),
+        columns=devices_dataframe.columns, index=devices_dataframe.columns
+    )
+
+    # euclid_Array = df_euclid.T.to_numpy()
+
+    # Create a numpy array from dataframe
+    # devices_array = devices_dataframe.T.to_numpy()
+    # Calculate the correlational distance
+    # dist = squareform(pdist(devices_array, metric='chebyshev'))
+    # Return data to a Panda dataframe
+    # values = pd.DataFrame(data=dist, index=list(devices.keys()),
+    #                       columns=list(devices.keys()))
+
+    # plt.figure(figsize=(8, 8))
+    # plt.imshow(euclid_Array, interpolation='none', cmap='jet')
+    # plt.colorbar()
+    # plt.savefig('correlation.png')
+
+    palette = sns.diverging_palette(20, 220, n=256)
+    ax = sns.heatmap(df_euclid, cmap=palette, vmax=1, vmin=0, square=True, annot=True)
+
+    ax.set_xticklabels(
+        ax.get_xticklabels(),
+        rotation=45,
+        horizontalalignment='right',
+    )
+
+    ax.set_yticklabels(
+        ax.get_yticklabels(),
+        rotation=0,
+        horizontalalignment='right',
+    )
+
+    plt.title('Matriz de correção entre os modelos de SENSORES')
+    plt.xlabel('Nome dos Sensores')
+    plt.ylabel('Nome dos Sensores')
+
+    plt.savefig('correlation.png')
+
+    return
 
 
 def nltk_ignition():
@@ -141,4 +199,3 @@ def remove_punctuation(content_with_punctuation):
 
     content_without_punctuation = concat[0:(len(concat) - 1)]
     return content_without_punctuation
-
